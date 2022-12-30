a230235a88732e9ec3f720f6750fb7e093f9d970
Adam Wehunt <adamwehunt@Adams-MacBook-Pro.local>
2022-12-30 12:25:24 -0800

    Init app.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,24 @@
-import './globals.css'
+import { Varela_Round } from '@next/font/google';
+import './globals.css';
+
+const varelaRound = Varela_Round({
+	weight: '400',
+	subsets: ['latin'],
+});
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      {/*
+	return (
+		<html lang="en" className={varelaRound.className}>
+			{/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+			<head />
+			<body>{children}</body>
+		</html>
+	);
 }
