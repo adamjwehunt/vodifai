554ed7302e98a7199ed757d75be2c3765e25d12a
Adam Wehunt <adamwehunt@Marisas-MacBook-Pro.local>
2023-03-16 22:10:07 -0700

    Remove text const.

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,12 @@ import SearchIcon from '@/public/search-icon.svg';
 import { Categories } from './components/Categories';
 import styles from './page.module.scss';
 
-const SEARCH_HEADER = 'Search';
 export const SEARCH_BAR_PLACEHOLDER = 'What do you want to watch?';
 
 export default function Home() {
 	return (
 		<div className={styles.searchHeaderWrapper}>
-			<h1 className={styles.searchHeader}>{SEARCH_HEADER}</h1>
+			<h1 className={styles.searchHeader}>{'Search'}</h1>
 			<Link href="/search">
 				<button className={styles.searchButton}>
 					<SearchIcon className={styles.searchIcon} />
