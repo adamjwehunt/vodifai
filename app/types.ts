54802c09a097a689bf850157071dd1c0c3847f13
Adam Wehunt <adamjwehunt@gmail.com>
2025-02-14 09:17:44 -0700

    Remove unused code.

--- app/types.ts
+++ app/types.ts
@@ -95,15 +95,3 @@ export interface Downloads {
 	videoFormats: Download[];
 	audioFormats: Download[];
 }
-
-export interface OpenAIStreamPayload {
-	model: string;
-	prompt: string;
-	temperature: number;
-	top_p: number;
-	frequency_penalty: number;
-	presence_penalty: number;
-	max_tokens: number;
-	stream: boolean;
-	n: number;
-}
